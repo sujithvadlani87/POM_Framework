0beab70219493e131ed821940b9d2a8c0730f13d
tm <tm@tm5>
2018-07-25 11:21:54 +0530

    New Project Commit

---
+++ src/main/java/Testing/Testing_Application/App.java
@@ -0,0 +1,13 @@
+package Testing.Testing_Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
